1b95613ad382b7fa72dc639b168edee6744e4db7
Arsalan Bashir <arsalanbashir831@gmail.com>
2022-08-27 14:29:43 +0500

    chngings

--- my-app/src/components/DivScroll.js
+++ my-app/src/components/DivScroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { AppBar, Toolbar } from '@mui/material';
 
 const DivScroll = ()=>{
-    let bgColor=getComputedStyle(document.documentElement).getPropertyValue("--components-light-theme");
+    let bgColor=getComputedStyle(document.documentElement).getPropertyValue("--components-dark-theme");
     return (
         <React.Fragment>
             <AppBar id = 'navbar' style={{zIndex:'1', top:'-70px'}} position = 'fixed' sx= {{background: bgColor, maxHeight:'70px'}}>
