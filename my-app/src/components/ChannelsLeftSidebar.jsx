f7d863a7b8fd3e3f633b661da424aa3f8227d7df
huzaifa003 <buntyhuzaifa3@gmail.com>
2022-08-28 15:32:28 +0500

    Added the List Items

--- my-app/src/components/ChannelsLeftSidebar.jsx
+++ my-app/src/components/ChannelsLeftSidebar.jsx
@@ -10,14 +10,15 @@ import DraftsIcon from '@mui/icons-material/Drafts';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
-import {Languages} from '../Data/Languages'
+import { Languages } from '../Data/Languages'
+import './ChannelsLeftSidebar.css'
 const ChannelsLeftSidebar = () => {
     const [selectedIndex, setSelectedIndex] = React.useState(0);
     return (
         <div class="ccc_column ccc_countryList">
             <div class="v-virtual-scroll">
-                <div class="v-virtual-scroll__container" style={{ height: '3195px' }}>
-                    <Box sx={{ flexGrow: 1 }}>
+                <div class="v-virtual-scroll__container" style={{ height: '3195px', alignContent:'center' }}>
+                    <Box sx={{ flexGrow: 1 , margin:'5%'}}>
                         <Grid container spacing={2}>
                             <Grid item xs={6} md={4}>
                                 <div
@@ -26,39 +27,29 @@ const ChannelsLeftSidebar = () => {
                                         {Languages.map((element, index) => {
                                             console.log(element);
                                             console.log(index);
-                                            
+
                                             return (
-                                            <ListItem onClick={()=>{setSelectedIndex(index)}} key={index} id={index} disablePadding sx={{ padding: 0.3 }} >
-                                                <ListItemButton sx={{ background: 'pink', borderRadius: 2 }}>
-                                                    <ListItemText primary={element} />
-                                                    {selectedIndex === index ?
-                                                    <ListItemIcon>
-                                                        <ArrowForwardIosRoundedIcon />
-                                                    </ListItemIcon>
-                                                    :
-                                                    ''
-                                                    }
-                                                </ListItemButton>
-                                            </ListItem>
+
+                                                <ListItem onClick={() => { setSelectedIndex(index) }} key={index} id={index} disablePadding sx={{ padding: 0.3 }} >
+                                                    <div className={selectedIndex === index ? 'divSelected': 'divUnselected'}>
+                                                        <ListItemButton  sx={{ background: 'white', borderRadius: 2 }}>
+                                                            <ListItemText primary={element} />
+                                                            {selectedIndex === index ?
+                                                                <ListItemIcon style={{paddingLeft:'50%'}} >
+                                                                    <ArrowForwardIosRoundedIcon />
+                                                                </ListItemIcon>
+                                                                :
+                                                                ''
+                                                            }
+                                                        </ListItemButton>
+                                                    </div>
+                                                </ListItem>
+
                                             )
+
                                         })}
-                                        <ListItem disablePadding sx={{ padding: 0.3 }} >
-                                            <ListItemButton sx={{ background: 'pink', borderRadius: 2 }}>
-                                                <ListItemText primary="Inbox" />
-                                                <ListItemIcon>
-                                                    <ArrowForwardIosRoundedIcon />
-                                                </ListItemIcon>
-                                            </ListItemButton>
-                                        </ListItem>
-                                        <ListItem disablePadding sx={{ padding: 0.3 }}  >
-                                            <ListItemButton sx={{ background: 'pink', borderRadius: 2 }}>
-                                                <ListItemText primary="Drafts" />
-                                                <ListItemIcon>
-                                                    <DraftsIcon />
-                                                </ListItemIcon>
-                                            </ListItemButton>
-                                        </ListItem>
-                                        
+
+
                                     </List>
                                 </div>
                             </Grid>
