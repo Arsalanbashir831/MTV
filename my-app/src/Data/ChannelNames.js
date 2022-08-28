bcac86e059cdf30570e1852e19231e83bf41d58b
huzaifa003 <buntyhuzaifa3@gmail.com>
2022-08-28 23:35:08 +0500

    Added dynamic categories

---
+++ my-app/src/Data/ChannelNames.js
@@ -0,0 +1,33 @@
+export const ChannelNames = [
+    [
+        "xyz",
+        "abc",
+        "lunatic",
+        "temp",
+        "etc"
+    ],
+    [
+        "NrbKaRZuKHSzPMGRkUPC",
+        "WNKWUrMJdxVMCJUVpaej",
+        "CoMEHOdjKpqPDatUxPoH",
+        "zuJbXbNsGAUucBSxRGcO",
+        "seYNoerXhwdTQVjqNFyz",
+        "ldysJvCVlykPyLhmklIn",
+        "PAwXsalANnvKFuphdpjh",
+        "gppzjTPRiRWgsMGezWOI",
+        "XUzMcKPVompViZSPEZvD",
+        "CWigFachRHzOApTTNqnD"
+    ],
+    [
+        "xyz",
+        "abc",
+        "lunatic",
+        "seYNoerXhwdTQVjqNFyz",
+        "ldysJvCVlykPyLhmklIn",
+        "PAwXsalANnvKFuphdpjh",
+        "gppzjTPRiRWgsMGezWOI",
+        "XUzMcKPVompViZSPEZvD",
+        "CWigFachRHzOApTTNqnD"
+    ]
+
+]
--- my-app/src/components/ChannelsGrid.jsx
+++ my-app/src/components/ChannelsGrid.jsx
@@ -12,8 +12,17 @@ import Box from '@mui/material/Box';
 import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
 import { Languages } from '../Data/Languages'
 import './ChannelsGrid.css'
+import { ChannelNames } from './../Data/ChannelNames'
 const ChannelsGrid = () => {
+
     const [selectedIndex, setSelectedIndex] = React.useState(0);
+    const [firstHalfArray, setFirstHalfArray] = React.useState([...ChannelNames[0]].splice(0, Math.ceil(ChannelNames[0].length / 2)))
+    const [secondHalfArray, setSecondHalfArray] = React.useState([...ChannelNames[0]].splice(Math.ceil(ChannelNames[0].length / 2)))
+    const handleChange = (index) => {
+        setSelectedIndex(index);
+        setFirstHalfArray([...ChannelNames[index]].splice(0, Math.ceil(ChannelNames[index].length / 2)))
+        setSecondHalfArray([...ChannelNames[index]].splice(Math.ceil(ChannelNames[index].length / 2)))
+    }
     return (
         <div class="ccc_column ccc_countryList">
             <div class="v-virtual-scroll">
@@ -31,7 +40,7 @@ const ChannelsGrid = () => {
 
                                             return (
 
-                                                <ListItem onClick={() => { setSelectedIndex(index) }} key={index} id={index} disablePadding sx={{ padding: 0.2 }} >
+                                                <ListItem onClick={() => { handleChange(index) }} key={index} id={index} disablePadding sx={{ padding: 0.2 }} >
                                                     <div className={selectedIndex === index ? 'divSelected' : 'divUnselected'}>
                                                         <ListItemButton sx={{ background: 'white', borderRadius: 2 }}>
                                                             <ListItemText style={{ whiteSpace: 'nowrap' }} primary={temp} />
@@ -55,18 +64,45 @@ const ChannelsGrid = () => {
                                 </div>
                             </Grid>
                             <Grid item xs={6} md={8}>
-                                <div style={{ background: '#030e2e', minHeight: '806px', maxHeight: '806px', overflow: 'auto', borderRadius: 6 }}>
+                                <div style={{ background: '#fff', minHeight: '806px', maxHeight: '806px', overflow: 'auto', borderRadius: 6 }}>
                                     <div class="ccc_column ccc_channelList">
                                         <div class="ccc_searchBar">
                                             <input type="text" name="searchbar" placeholder="Search your favorite channel here …" />
                                         </div>
-                                    </div>
-                                    <Box sx={{ flexGrow: 1}}>
-                                        <Grid container spacing={1}>
-                                            
 
-                                        </Grid>
-                                    </Box>
+                                        <Box sx={{ flexGrow: 1 }}>
+                                            <Grid container rowSpacing={3} columnSpacing={{ xs: 1, sm: 1, md: 1 }}>
+                                                <Grid container item xs={6} direction="row">
+                                                    <div className='divUnselected'>
+                                                        <List disablePadding >
+                                                            {firstHalfArray.map((element, index) => {
+                                                                return (
+                                                                    <ListItem key={index} disablePadding sx={{ background: 'purple', borderRadius: 2, padding: 0.7, marginTop: 1 }}>
+                                                                        <ListItemText primary={element} />
+                                                                    </ListItem>
+                                                                )
+                                                            })}
+
+                                                        </List>
+                                                    </div>
+                                                </Grid>
+
+                                                <Grid container item xs={6} direction="column">
+                                                    <List disablePadding >
+                                                        {secondHalfArray.map((element, index) => {
+                                                            return (
+                                                                <ListItem key={index} disablePadding sx={{ background: 'purple', borderRadius: 2, padding: 0.7, marginTop: 1 }}>
+                                                                    <ListItemText primary={element} />
+                                                                </ListItem>
+                                                            )
+                                                        })}
+
+                                                    </List>
+                                                </Grid>
+
+                                            </Grid>
+                                        </Box>
+                                    </div>
 
                                 </div>
                             </Grid>
