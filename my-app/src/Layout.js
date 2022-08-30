24caccc28f09bf7323aeb8c1fb8a8d02e7646a61
Arsalan Bashir <arsalanbashir831@gmail.com>
2022-08-30 17:40:49 +0500

    comit

--- my-app/src/Layout.js
+++ my-app/src/Layout.js
@@ -4,11 +4,13 @@ import Box from '@mui/material/Box';
 
 import Home from './Pages/Home';
 import Footer from './components/Footer';
+import Main from './Pages/Main';
 const Layout = () => {
   return (
     <React.Fragment>
       {/* Routes will be initialized here  */}
-          <Home></Home>
+          {/* <Home></Home> */}
+          <Main></Main>
     </React.Fragment>
   )
 }
