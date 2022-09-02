7286dfe603e16c0d3a15c30dc0d26cbeedd9d808
Arsalan Bashir <arsalanbashir831@gmail.com>
2022-09-02 05:06:45 +0500

    comit

--- my-app/src/Pages/Channelv2.js
+++ my-app/src/Pages/Channelv2.js
@@ -5,7 +5,7 @@ import ChannelHeadv2 from '../components/ChannelHeadv2'
 const Channelv2 = () => {
   return (
     <div id='ccc_channels' style={{    padding: '107px 0 57px'}}> 
-  <ChannelHeadv2></ChannelHeadv2>
+  {/* <ChannelHeadv2></ChannelHeadv2> */}
     <ChannelGridv2></ChannelGridv2>
     </div>
   )
--- my-app/src/components/ChannelGridv2.js
+++ my-app/src/components/ChannelGridv2.js
@@ -3,15 +3,300 @@ import './ChannelGridv2.css';
 const ChannelGridv2 = () => {
   return (
    <>
-   <section id='ccc_channels_list'>
-    <div className='ccc_container'>
-        <div className='ccc_column ccc_countryList'>
-            <div className='virtual-scroll'>
-                
+  <div id="DIV_1">
+  <section id="SECTION_2">
+    <h1 id="H1_3">
+      Our Full IPTV Channels List
+    </h1>
+    <h2 id="H2_4">
+      An easy access and super simple IPTV service for all your devices.
+    </h2>
+  </section>
+  <section id="SECTION_5">
+    <div id="DIV_6">
+      <div id="DIV_7">
+        <div id="DIV_8">
+          <div id="DIV_9">
+            <div id="DIV_10">
+              <div id="DIV_11">
+                ARGENTINA
+              </div>
             </div>
+            <div id="DIV_12">
+              <div id="DIV_13">
+                MEXICO
+              </div>
+            </div>
+            <div id="DIV_14">
+              <div id="DIV_15">
+                COLUMBIA
+              </div>
+            </div>
+            <div id="DIV_16">
+              <div id="DIV_17">
+                URUGUAY
+              </div>
+            </div>
+            <div id="DIV_18">
+              <div id="DIV_19">
+                ECUADOR
+              </div>
+            </div>
+            <div id="DIV_20">
+              <div id="DIV_21">
+                PERU
+              </div>
+            </div>
+            <div id="DIV_22">
+              <div id="DIV_23">
+                CHILE
+              </div>
+            </div>
+            <div id="DIV_24">
+              <div id="DIV_25">
+                BOLIVIA
+              </div>
+            </div>
+            <div id="DIV_26">
+              <div id="DIV_27">
+                LATINO - CINE
+              </div>
+            </div>
+            <div id="DIV_28">
+              <div id="DIV_29">
+                LATINO - DEPORTE
+              </div>
+            </div>
+            <div id="DIV_30">
+              <div id="DIV_31">
+                MLB
+              </div>
+            </div>
+            <div id="DIV_32">
+              <div id="DIV_33">
+                NHL
+              </div>
+            </div>
+            <div id="DIV_34">
+              <div id="DIV_35">
+                NFL
+              </div>
+            </div>
+            <div id="DIV_36">
+              <div id="DIV_37">
+                NBA
+              </div>
+            </div>
+            <div id="DIV_38">
+              <div id="DIV_39">
+                COSTA RICA
+              </div>
+            </div>
+            <div id="DIV_40">
+              <div id="DIV_41">
+                REPUBLICA DOMINICANA
+              </div>
+            </div>
+            <div id="DIV_42">
+              <div id="DIV_43">
+                VENEZUELA
+              </div>
+            </div>
+            <div id="DIV_44">
+              <div id="DIV_45">
+                LATINO - 24/7 SERIES
+              </div>
+            </div>
+            <div id="DIV_46">
+              <div id="DIV_47">
+                PEACOCK
+              </div>
+            </div>
+            <div id="DIV_48">
+              <div id="DIV_49">
+                Scottish Premiership
+              </div>
+            </div>
+            <div id="DIV_50">
+              <div id="DIV_51">
+                ESPANA - FOOTTERS
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
+      <div id="DIV_52">
+        <div id="DIV_53">
+          <input type="text" name="searchbar" placeholder="Search your favorite channel here …" id="INPUT_54" />
+        </div>
+        <div id="DIV_55">
+          <div id="DIV_56">
+            <div id="DIV_57">
+              <div id="DIV_58">
+                COLOMBIA SD | CITY TV
+              </div>
+              <div id="DIV_59">
+                COLOMBIA HD | CARACOL
+              </div>
+            </div>
+            <div id="DIV_60">
+              <div id="DIV_61">
+                COLOMBIA HD | RCN
+              </div>
+              <div id="DIV_62">
+                COLOMBIA HD | RCN TELENOVELA
+              </div>
+            </div>
+            <div id="DIV_63">
+              <div id="DIV_64">
+                COLOMBIA HD | TELEPACIFICO
+              </div>
+              <div id="DIV_65">
+                COLOMBIA SD | TELEANTIOQUIA
+              </div>
+            </div>
+            <div id="DIV_66">
+              <div id="DIV_67">
+                COLOMBIA HD | TELEMEDELLIN
+              </div>
+              <div id="DIV_68">
+                COLOMBIA HD | SEÑAL COLOMBIA
+              </div>
+            </div>
+            <div id="DIV_69">
+              <div id="DIV_70">
+                WIN SPORTS | HD
+              </div>
+              <div id="DIV_71">
+                WIN SPORTS | HD
+              </div>
+            </div>
+            <div id="DIV_72">
+              <div id="DIV_73">
+                WIN SPORTS + | SD
+              </div>
+              <div id="DIV_74">
+                COLOMBIA HD | NTN24
+              </div>
+            </div>
+            <div id="DIV_75">
+              <div id="DIV_76">
+                COLOMBIA SD | MUNDO +
+              </div>
+              <div id="DIV_77">
+                COLOMBIA HD | TELECARIBE
+              </div>
+            </div>
+            <div id="DIV_78">
+              <div id="DIV_79">
+                COLOMBIA LOCAL | CANAL CAPITAL
+              </div>
+              <div id="DIV_80">
+                COLOMBIA LOCAL | CANAL TRECE
+              </div>
+            </div>
+            <div id="DIV_81">
+              <div id="DIV_82">
+                COLOMBIA LOCAL | SUDAN
+              </div>
+              <div id="DIV_83">
+                COLOMBIA LOCAL | AMORDISCOS TV
+              </div>
+            </div>
+            <div id="DIV_84">
+              <div id="DIV_85">
+                COLOMBIA LOCAL | CALI TV
+              </div>
+              <div id="DIV_86">
+                COLOMBIA LOCAL | CANAL CNC
+              </div>
+            </div>
+            <div id="DIV_87">
+              <div id="DIV_88">
+                COLOMBIA LOCAL | CNC MEDELLIN
+              </div>
+              <div id="DIV_89">
+                COLOMBIA LOCAL | CANAL CAPITAL
+              </div>
+            </div>
+            <div id="DIV_90">
+              <div id="DIV_91">
+                COLOMBIA LOCAL | COSMOVISION
+              </div>
+              <div id="DIV_92">
+                COLOMBIA LOCAL | TV AGRO
+              </div>
+            </div>
+            <div id="DIV_93">
+              <div id="DIV_94">
+                COLOMBIA LOCAL | TV GRACIA
+              </div>
+              <div id="DIV_95">
+                COLOMBIA LOCAL | BUENISIMA RADIO TV
+              </div>
+            </div>
+            <div id="DIV_96">
+              <div id="DIV_97">
+                COLOMBIA LOCAL | TV PEÑOL
+              </div>
+              <div id="DIV_98">
+                COLOMBIA LOCAL | RECUERDOS RETRO RADIO TV
+              </div>
+            </div>
+            <div id="DIV_99">
+              <div id="DIV_100">
+                COLOMBIA LOCAL | TELE PASTO
+              </div>
+              <div id="DIV_101">
+                COLOMBIA LOCAL | TV GRACIA
+              </div>
+            </div>
+            <div id="DIV_102">
+              <div id="DIV_103">
+                COLOMBIA LOCAL | TELEFAMILIA
+              </div>
+              <div id="DIV_104">
+                COLOMBIA LOCAL | MELODY CHANNEL
+              </div>
+            </div>
+            <div id="DIV_105">
+              <div id="DIV_106">
+                COLOMBIA LOCAL | CTV
+              </div>
+              <div id="DIV_107">
+                COLOMBIA LOCAL | TDI
+              </div>
+            </div>
+            <div id="DIV_108">
+              <div id="DIV_109">
+                ECUADOR LOCAL | ASOMAVISION
+              </div>
+              <div id="DIV_110">
+                COLOMBIA LOCAL | CANAL DOCE SINCELEJO
+              </div>
+            </div>
+            <div id="DIV_111">
+              <div id="DIV_112">
+                COLOMBIA LOCAL | CANAL CNC PASTO
+              </div>
+              <div id="DIV_113">
+                COLOMBIA LOCAL | CANAL INSTITUCIONAL
+              </div>
+            </div>
+            <div id="DIV_114">
+              <div id="DIV_115">
+                COLOMBIA LOCAL | CANAL TRECE
+              </div>
+              <div id="DIV_116">
+                COLOMBIA LOCAL | CANAL VISION DORADA
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-   </section>
+  </section>
+</div>
    </>
   )
 }
