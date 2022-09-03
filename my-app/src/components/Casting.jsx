2bd7e11a673a55436b41522adf534da7ba7cec01
huzaifa003 <buntyhuzaifa3@gmail.com>
2022-09-04 00:40:11 +0500

    stupid casting

--- my-app/src/components/Casting.jsx
+++ my-app/src/components/Casting.jsx
@@ -1,23 +1,32 @@
 import chromecast from '../assets/chromecast.svg'
 import airplay from '../assets/airplay.svg'
 import miracast from '../assets/miracast.png'
+
+import './Casting.css'
 const Casting = (props) => {
     return (
         <>
-            
-                <div id= 'ccc_cast'>
+            <div className='v-window__container' style={{maxWidth:'763px'}}>
+                {/* <div id= 'ccc_cast'>
                     <div className='ccc_box'>
                         <h2 class="ccc_title">Cast our IPTV with any TV screen.</h2>
                         <h2 class="ccc_subtitle">Casting work on most apps and allows you to stream content from your smart device.</h2>
                     </div>
 
                     <div className="ccc_box">
-                        <p className="ccc_name">Google Chromecast.</p>
+                        <p id='paragraph' class="ccc_name">Google Chromecast.</p>
                         <img src= {chromecast} alt="chromecast" class="ccc_img"></img>
                     </div>
-                </div>
+                </div> */}
 
-            
+                
+                <div id="ccc_cast"><div className="ccc_box"><h2 className="ccc_title">Cast our IPTV with any TV screen.</h2> <h2 className="ccc_subtitle">Casting work on most apps and allows you to stream content from your smart device.</h2></div> <div className="ccc_box"><p className="ccc_name">Google Chromecast.</p> <img src={chromecast} alt="chromecast" className="ccc_img" /></div> <div className="ccc_box"><p className="ccc_name">APPLE airplay</p> <img src={airplay} alt="airplay" className="ccc_img" /></div> <div className="ccc_box"><p className="ccc_name">MIRACAST</p> <img src={miracast} alt="miracast" className="ccc_img" /></div> <div className="ccc_box"><div className="ccc_notice"><h2 className="ccc_subtitle">
+                    *Casting only works if devices are connected to the same local network.
+                </h2></div></div></div>
+
+
+
+            </div>
         </>
     )
 }
