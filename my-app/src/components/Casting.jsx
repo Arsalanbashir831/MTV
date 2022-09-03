d85d066b5d95ee42e2f8e1313987274ceaf14c6c
huzaifa003 <buntyhuzaifa3@gmail.com>
2022-09-03 23:32:00 +0500

    trial

--- my-app/src/components/Casting.jsx
+++ my-app/src/components/Casting.jsx
@@ -4,23 +4,20 @@ import miracast from '../assets/miracast.png'
 const Casting = (props) => {
     return (
         <>
-            <div className='v-window__container'>
-                {/* <div id= 'ccc_cast'>
+            
+                <div id= 'ccc_cast'>
                     <div className='ccc_box'>
                         <h2 class="ccc_title">Cast our IPTV with any TV screen.</h2>
                         <h2 class="ccc_subtitle">Casting work on most apps and allows you to stream content from your smart device.</h2>
                     </div>
 
                     <div className="ccc_box">
-                        <p id='paragraph' class="ccc_name">Google Chromecast.</p>
+                        <p className="ccc_name">Google Chromecast.</p>
                         <img src= {chromecast} alt="chromecast" class="ccc_img"></img>
                     </div>
-                </div> */}
+                </div>
 
-                <div id="ccc_cast"><div className="ccc_box"><h2 className="ccc_title">Cast our IPTV with any TV screen.</h2> <h2 className="ccc_subtitle">Casting work on most apps and allows you to stream content from your smart device.</h2></div> <div className="ccc_box"><p className="ccc_name">Google Chromecast.</p> <img src={chromecast} alt="chromecast" className="ccc_img"/></div> <div className="ccc_box"><p className="ccc_name">APPLE airplay</p> <img src={airplay} alt="airplay" className="ccc_img"/></div> <div className="ccc_box"><p className="ccc_name">MIRACAST</p> <img src={miracast} alt="miracast" className="ccc_img"/></div> <div className="ccc_box"><div className="ccc_notice"><h2 className="ccc_subtitle">
-                    *Casting only works if devices are connected to the same local network.
-                </h2></div></div></div>
-            </div>
+            
         </>
     )
 }
