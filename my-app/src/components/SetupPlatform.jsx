a0006d243e1e6c62e49414a423603ec5c03afb4f
huzaifa003 <buntyhuzaifa3@gmail.com>
2022-09-04 01:24:19 +0500

    added Scrolling

--- my-app/src/components/SetupPlatform.jsx
+++ my-app/src/components/SetupPlatform.jsx
@@ -23,9 +23,11 @@ const SetupPlateform = () => {
   const [selectedIndex, setSelectedIndex] = React.useState(0);
   const iconSize = 122;
 
-  const handleChangeIndex = (index)=>{
-    console.log(index)
-    setSelectedIndex(index)
+  const handleChangeIndex = (index) => {
+    console.log(document.getElementsByClassName('bottomContainer'))
+    setSelectedIndex(index);
+    document.getElementsByClassName('mainContainerDesktop')[0].scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"})
+    window.scrollBy(0,-60)
   }
   return (
 
@@ -42,15 +44,15 @@ const SetupPlateform = () => {
           }}
 
         >
-          <Card  id={0} className= {0== selectedIndex ? '' : 'unSelectedCard'} onClick={() => { handleChangeIndex(0)}}  sx={{ maxWidth: 400, maxHeight: '170px' }} >
+          <Card id={0} className={0 == selectedIndex ? '' : 'unSelectedCard'} onClick={() => { handleChangeIndex(0) }} sx={{ maxWidth: 400, maxHeight: '170px' }} >
             <CardActionArea>
               <div style={{ padding: 4 }}>
-                <div className= 'centerContent'>
-                  <MdAccessibilityNew className= {0 === selectedIndex ? '' : 'selectedWhite'} size={iconSize} />
+                <div className='centerContent'>
+                  <MdAccessibilityNew className={0 === selectedIndex ? '' : 'selectedWhite'} size={iconSize} />
                 </div>
-                <div className= 'centerContent'>
+                <div className='centerContent'>
                   <CardContent sx={{ padding: 0 }}>
-                    <Typography sx={{ padding: 0 }} gutterBottom variant="h6" component="div" className= {0 === selectedIndex ? '' : 'selectedWhite'}>
+                    <Typography sx={{ padding: 0 }} gutterBottom variant="h6" component="div" className={0 === selectedIndex ? '' : 'selectedWhite'}>
                       Lizard
                     </Typography>
                   </CardContent>
@@ -59,15 +61,15 @@ const SetupPlateform = () => {
             </CardActionArea>
           </Card>
 
-          <Card id={1} className= {1 === selectedIndex ? '' : 'unSelectedCard'} onClick={() => { handleChangeIndex(1)}}  sx={{ maxWidth: 400, maxHeight: '170px' }} >
+          <Card id={1} className={1 === selectedIndex ? '' : 'unSelectedCard'} onClick={() => { handleChangeIndex(1) }} sx={{ maxWidth: 400, maxHeight: '170px' }} >
             <CardActionArea>
               <div style={{ padding: 4 }}>
-                <div className= 'centerContent'>
-                  <MdAccessibilityNew size={iconSize} className= {1=== selectedIndex ? '' : 'selectedWhite'} />
+                <div className='centerContent'>
+                  <MdAccessibilityNew size={iconSize} className={1 === selectedIndex ? '' : 'selectedWhite'} />
                 </div>
-                <div className= 'centerContent'>
+                <div className='centerContent'>
                   <CardContent sx={{ padding: 0 }}>
-                    <Typography className= {1=== selectedIndex ? '' : 'selectedWhite'} sx={{ padding: 0 }} gutterBottom variant="h6" component="div">
+                    <Typography className={1 === selectedIndex ? '' : 'selectedWhite'} sx={{ padding: 0 }} gutterBottom variant="h6" component="div">
                       Lizard
                     </Typography>
                   </CardContent>
@@ -76,15 +78,15 @@ const SetupPlateform = () => {
             </CardActionArea>
           </Card>
 
-          <Card id= {2} className= { 2=== selectedIndex ? '' : 'unSelectedCard'} onClick={() => { handleChangeIndex(2)}}  sx={{ maxWidth: 400, maxHeight: '170px' }} >
+          <Card id={2} className={2 === selectedIndex ? '' : 'unSelectedCard'} onClick={() => { handleChangeIndex(2) }} sx={{ maxWidth: 400, maxHeight: '170px' }} >
             <CardActionArea>
-              <div style={{ padding: 4 , zIndex : 10 }}>
-                <div className= 'centerContent'>
-                  <MdAccessibilityNew className= {2 === selectedIndex ? '' : 'selectedWhite'} size={iconSize} />
+              <div style={{ padding: 4, zIndex: 10 }}>
+                <div className='centerContent'>
+                  <MdAccessibilityNew className={2 === selectedIndex ? '' : 'selectedWhite'} size={iconSize} />
                 </div>
-                <div className= 'centerContent'>
+                <div className='centerContent'>
                   <CardContent sx={{ padding: 0 }}>
-                    <Typography className= {2=== selectedIndex ? '' : 'selectedWhite'} sx={{ padding: 0 }} gutterBottom variant="h6" component="div">
+                    <Typography className={2 === selectedIndex ? '' : 'selectedWhite'} sx={{ padding: 0 }} gutterBottom variant="h6" component="div">
                       Lizard
                     </Typography>
                   </CardContent>
@@ -93,15 +95,15 @@ const SetupPlateform = () => {
             </CardActionArea>
           </Card>
 
-          <Card id={3} className= {3 === selectedIndex ? '' : 'unSelectedCard'} onClick={() => { handleChangeIndex(3)}}  sx={{ maxWidth: 400, maxHeight: '170px' }} >
+          <Card id={3} className={3 === selectedIndex ? '' : 'unSelectedCard'} onClick={() => { handleChangeIndex(3) }} sx={{ maxWidth: 400, maxHeight: '170px' }} >
             <CardActionArea>
               <div style={{ padding: 4 }}>
-                <div className= 'centerContent'>
-                  <MdAccessibilityNew className= {3 === selectedIndex ? '' : 'selectedWhite'} size={iconSize} />
+                <div className='centerContent'>
+                  <MdAccessibilityNew className={3 === selectedIndex ? '' : 'selectedWhite'} size={iconSize} />
                 </div>
-                <div className= 'centerContent'>
+                <div className='centerContent'>
                   <CardContent sx={{ padding: 0 }}>
-                    <Typography className= {3 === selectedIndex ? '' : 'selectedWhite'} sx={{ padding: 0 }} gutterBottom variant="h6" component="div">
+                    <Typography className={3 === selectedIndex ? '' : 'selectedWhite'} sx={{ padding: 0 }} gutterBottom variant="h6" component="div">
                       Lizard
                     </Typography>
                   </CardContent>
@@ -110,15 +112,15 @@ const SetupPlateform = () => {
             </CardActionArea>
           </Card>
 
-          <Card id={4} className= {4 === selectedIndex ? '' : 'unSelectedCard'} onClick={() => { handleChangeIndex(4)}}  sx={{ maxWidth: 400, maxHeight: '170px' }} >
+          <Card id={4} className={4 === selectedIndex ? '' : 'unSelectedCard'} onClick={() => { handleChangeIndex(4) }} sx={{ maxWidth: 400, maxHeight: '170px' }} >
             <CardActionArea>
               <div style={{ padding: 4 }}>
-                <div className= 'centerContent'>
-                  <MdAccessibilityNew className= {4 === selectedIndex ? '' : 'selectedWhite'} size={iconSize} />
+                <div className='centerContent'>
+                  <MdAccessibilityNew className={4 === selectedIndex ? '' : 'selectedWhite'} size={iconSize} />
                 </div>
-                <div className= 'centerContent'>
+                <div className='centerContent'>
                   <CardContent sx={{ padding: 0 }}>
-                    <Typography className= {4 === selectedIndex ? '' : 'selectedWhite'} sx={{ padding: 0 }} gutterBottom variant="h6" component="div">
+                    <Typography className={4 === selectedIndex ? '' : 'selectedWhite'} sx={{ padding: 0 }} gutterBottom variant="h6" component="div">
                       Lizard
                     </Typography>
                   </CardContent>
@@ -127,21 +129,21 @@ const SetupPlateform = () => {
             </CardActionArea>
           </Card>
         </Box>
-  
-          
-          
-          <div className= 'bottomContainer'>
+
+
+
+        <div className='bottomContainer'>
           <SwipeableViews index={selectedIndex} >
-          <div> <WebSetup></WebSetup> </div>
-          <div> {1===selectedIndex?<MobSetup></MobSetup>:''} </div>
-          <div>  {2===selectedIndex?<Tv></Tv> :''} </div>
-          <div>  {3===selectedIndex?<BoxStick></BoxStick> :''} </div>
-          <div> <h1 style={{color: 'yellow'}}>heel</h1> </div>
+            <div> {0 === selectedIndex ? <WebSetup></WebSetup> : ''} </div>
+            <div> {1 === selectedIndex ? <MobSetup></MobSetup> : ''} </div>
+            <div>  {2 === selectedIndex ? <Tv></Tv> : ''} </div>
+            <div>  {3 === selectedIndex ? <BoxStick></BoxStick> : ''} </div>
+            <div> <h1 style={{ color: 'yellow' }}>heel</h1> </div>
 
 
           </SwipeableViews>
-          </div>
-          
+        </div>
+
       </div>
     </>
   )
