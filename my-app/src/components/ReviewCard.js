053e48c7cf66b5f9008f2cbf8f2e55a0cf44849f
huzaifa003 <buntyhuzaifa3@gmail.com>
2022-09-05 03:28:51 +0500

    added review card

--- my-app/src/components/ReviewCard.js
+++ my-app/src/components/ReviewCard.js
@@ -13,7 +13,7 @@ const ReviewCard = (props) => {
           <img src={trustStar} alt="trust star"/>
           <img src={trustStar} alt="trust star"/> 
           </div> 
-          <p class="ccc_date">${props.days} day ago</p></div> 
+          <p class="ccc_date">{props.days} days ago</p></div> 
           <div class="ccc_body"><h1  class="ccc_title ">{props.title}</h1> 
           <h2 class="ccc_subtitle ">{props.subtitle}</h2></div> 
           <div class="ccc_footer"><p class="ccc_author">{props.author}</p>
