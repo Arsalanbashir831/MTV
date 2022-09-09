9ae2adb1ba8a1d7af262fd3c10ad5aae429b4eea
huzaifa003 <buntyhuzaifa3@gmail.com>
2022-09-10 00:53:03 +0500

    Added Option Toggle

--- my-app/src/Pages/OrderPage.js
+++ my-app/src/Pages/OrderPage.js
@@ -4,16 +4,46 @@ import SimpleAccordion from '../components/Devices'
 //import Label from '../components/Label'
 import './OrderPage.css'
 import Addons from '../components/Addons'
+
+import SwitchSelector from "react-switch-selector";
+const options = [
+  {
+    label: 'Option 1',
+    value: false,
+    selectedBackgroundColor: "#0097e6",
+  },
+  {
+    label: "Option 2",
+    value: true,
+    selectedBackgroundColor: "#fbc531"
+  }
+];
+
+const onChange = (newValue) => {
+  console.log(newValue);
+};
+
 const OrderPage = () => {
   return (
-   <>
-  
-  <div id='OrderSection'>
- <PackagePlans></PackagePlans>
- <SimpleAccordion></SimpleAccordion>
- <Addons></Addons>
-  </div>
-   </>
+    <>
+
+      <div id='OrderSection'>
+        <PackagePlans></PackagePlans>
+
+        <div style={{ width: "100%", height: 30, justifySelf:'center' }}>
+          <SwitchSelector
+            onChange={onChange}
+            options={options}
+            initialSelectedIndex={0}
+            backgroundColor={"#353b48"}
+            fontColor={"#f5f6fa"}
+          />
+        </div>
+
+        <SimpleAccordion></SimpleAccordion>
+        <Addons></Addons>
+      </div>
+    </>
   )
 }
 
