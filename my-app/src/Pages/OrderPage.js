8227f0dec037e95b5528b29a1a7a4795e8f32212
huzaifa003 <buntyhuzaifa3@gmail.com>
2022-09-11 22:24:45 +0500

    Completed URL SECTION

--- my-app/src/Pages/OrderPage.js
+++ my-app/src/Pages/OrderPage.js
@@ -22,23 +22,7 @@ const OrderPage = () => {
     return sum;
   }
   const [total, setTotal] = React.useState(calculateTotal());
-  const options = [
-    {
-      label: 'Option 1',
-      value: false,
-      selectedBackgroundColor: "linear-gradient(180deg,#37d348,#13cd7d)",
-    },
-    {
-      label: "Option 2",
-      value: true,
-      selectedBackgroundColor: "linear-gradient(180deg,#37d348,#13cd7d)"
-    }
-  ];
 
-  const onChange = (newValue) => {
-    console.log(newValue);
-    setSelection(!selection);
-  };
 
   const handleTotal = () => {
     setTotal(calculateTotal());
@@ -51,24 +35,7 @@ const OrderPage = () => {
 
       <div id='OrderSection'>
         <div onClick={handleTotal}><PackagePlans></PackagePlans></div>
-        <div style={{ marginBottom: '5%', marginTop: '5%' }} s><Label no={2} text={'Select the Number of Devices'} ></Label> </div>
-        <div style={{ width: "100%", height: 50, justifySelf: 'center' }}>
-          <SwitchSelector 
-            onChange={onChange}
-            options={options}
-            initialSelectedIndex={0}
-            // backgroundColor={"#353b48"}
-            backgroundColor={"#030e2e"}
-            fontColor={"#f5f6fa"}
-            wrapperBorderRadius={10}
-            optionBorderRadius={10}
-            fontSize={20}
-            
-         
-          />
-        </div>
-
-        <div onClick={handleTotal}><SimpleAccordion type={selection}></SimpleAccordion> </div>
+        <div onClick={handleTotal}><SimpleAccordion></SimpleAccordion> </div>
         <div onClick={handleTotal}> <Addons></Addons> </div>
         <div onClick={handleTotal}> <Total></Total> </div>
       </div>
--- my-app/src/components/Devices.jsx
+++ my-app/src/components/Devices.jsx
@@ -7,15 +7,16 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import DevicesCard from './DevicesCard'
 import Grid from '@mui/material/Grid';
 import Label from './Label';
+import SwitchSelector from "react-switch-selector";
 
 import TotalBill from './TotalBill';
 import TotalURL from './TotalURL';
 export default function SimpleAccordion(props) {
 
-  const priceMap= {
-    'dev1' : 5,
-    'dev2' : 10,
-    'dev3' : 15,
+  const priceMap = {
+    'dev1': 5,
+    'dev2': 10,
+    'dev3': 15,
   }
 
   const detailsMap = {
@@ -32,13 +33,26 @@ export default function SimpleAccordion(props) {
       'details': '3 Device'
     },
   }
+  
+  const [selection, setSelection] = React.useState(false);
   const [activatedId, setactivatedId] = React.useState('dev1');
   TotalBill.devicePrice = priceMap[activatedId];
   TotalURL.deviceDetails = detailsMap[activatedId];
+
+  if (selection == false){
+    TotalURL.typeDetails.url='mag';
+    TotalURL.typeDetails.value='Mag Type';
+  }
+
+  else{
+    TotalURL.typeDetails.url='';
+    TotalURL.typeDetails.value='';
+  }
   // handling the selective package 
   const clickHandler = (id) => {
     const element_prev = document.getElementById(activatedId);
     const element = document.getElementById(id);
+
 
     if (element_prev === null) {
       setactivatedId(id);
@@ -51,13 +65,63 @@ export default function SimpleAccordion(props) {
 
     TotalBill.devicePrice = priceMap[id];
     TotalURL.deviceDetails = detailsMap[id];
+    if (selection == false){
+      TotalURL.typeDetails.url='mag';
+      TotalURL.typeDetails.value='Mag Type';
+    }
+    else{
+      TotalURL.typeDetails.url='';
+      TotalURL.typeDetails.value='';
+    }
 
+    console.log(TotalURL.typeDetails);
+    console.log(TotalURL.deviceDetails);
   }
+
+  const options = [
+    {
+      label: 'Option 1',
+      value: false,
+      selectedBackgroundColor: "linear-gradient(180deg,#37d348,#13cd7d)",
+    },
+    {
+      label: "Option 2",
+      value: true,
+      selectedBackgroundColor: "linear-gradient(180deg,#37d348,#13cd7d)"
+    }
+  ];
+
+  const onChange = (newValue) => {
+    console.log(newValue);
+    setSelection(!selection);
+    if (selection == true){
+      clickHandler('dev1');
+      document.getElementById('deviceType').click();
+      TotalURL.typeDetails.url='mag';
+      TotalURL.typeDetails.value='Mag Type';
+    }
+  };
 
 
   return (
+  <>
+    <div id='deviceType' style={{ width: "100%", height: 50, justifySelf: 'center' }}>
+          <SwitchSelector 
+            onChange={onChange}
+            options={options}
+            initialSelectedIndex={0}
+            // backgroundColor={"#353b48"}
+            backgroundColor={"#030e2e"}
+            fontColor={"#f5f6fa"}
+            wrapperBorderRadius={10}
+            optionBorderRadius={10}
+            fontSize={20}
+            
+         
+          />
+        </div>
     <div id='DeviceSection'>
-      
+
       {/* <Accordion>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
@@ -84,14 +148,17 @@ export default function SimpleAccordion(props) {
               </Grid>
             </Grid>
           </Grid> */}
+
+          
       <div className="Packages-Container-SmallMobile" style={{ display: "flex", flexDirection: "column", gap: "2rem", padding: '40px 0px' }}>
-        <DevicesCard divId='dev1' active={activatedId} handleChange={() => clickHandler('dev1')} devices={`1  Device`} description="Use your IPTV on Single Device at a time" price={priceMap.dev1} duration="month" ></DevicesCard>
-        {props.type ?
+
+      <DevicesCard divId='dev1' active={activatedId} handleChange={() => clickHandler('dev1')} devices={`1  Device`} description="Use your IPTV on Single Device at a time" price={priceMap.dev1} duration="month" ></DevicesCard>
+        {selection ?
           <>
             <DevicesCard divId='dev2' active={activatedId} handleChange={() => clickHandler('dev2')} devices={`2   Devices`} description="Use your IPTV on Double Device at a time" price={priceMap.dev2} duration="month" ></DevicesCard>
-              <DevicesCard divId='dev3' active={activatedId} handleChange={() => clickHandler('dev3')} devices={`3   Devices`} description="Use your IPTV on Tripple Device at a time" price={priceMap.dev3} duration="month" ></DevicesCard>
+            <DevicesCard divId='dev3' active={activatedId} handleChange={() => clickHandler('dev3')} devices={`3   Devices`} description="Use your IPTV on Tripple Device at a time" price={priceMap.dev3} duration="month" ></DevicesCard>
           </>
-          : ''}
+          : <></>}
 
       </div>
       {/* 
@@ -99,5 +166,6 @@ export default function SimpleAccordion(props) {
       </Accordion> */}
 
     </div>
+    </>
   );
 }
